fca8640f8b33a55ed8e1c322cc06ab2158c355e4
Gaurav Singh <gauravsinghchunar007@gmail.com>
2025-10-04 18:35:29 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -7,7 +7,7 @@ const PORT = 5000;
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: 'https://gauravsingh-problem7frontend.onrender.com', 
+  origin: 'https://gauravsingh-problem7-1.onrender.com', 
   credentials: true
 }));
 
